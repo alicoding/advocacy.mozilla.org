ab5b4d252b65f585e434f2baed494fa0a3888246
Ali Al Dallal <ali@alicoding.com>
2016-01-27 16:24:12 -0500

    commit my stuff

--- components/header.jsx
+++ components/header.jsx
@@ -3,29 +3,41 @@ var Router = require('react-router');
 var Link = Router.Link;
 var ImageTag = require('./imagetag.jsx');
 
+var Menu = React.createClass({
+  render: function() {
+    return (
+      <div className="nav-items">
+        <div className="nav-link-container">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="nav-link-container">
+          <Link to="/open-web-fellows/">Open Web Fellows</Link>
+        </div>
+        <div className="nav-link-container">
+          <a href="">Blog</a>
+        </div>
+      </div>
+    );
+  }
+});
+
 module.exports = React.createClass({
   render: function() {
     var logoImage = this.props.logoImage || "/assets/logo-advocacy.svg";
+    var menu = (<Menu/>);
+    if (this.props.disabledMenu) {
+      menu = '';
+    }
     return (
       <div className="header">
         <div className="header-content">
           <div className="nav-home">
             <Link to="/">
-              <ImageTag src1x={logoImage}
+              <ImageTag width={this.props.width} height={this.props.height} src1x={logoImage}
                 alt="advocacy logo"/>
             </Link>
           </div>
-          <div className="nav-items">
-            <div className="nav-link-container">
-              <Link to="/">Home</Link>
-            </div>
-            <div className="nav-link-container">
-              <Link to="/open-web-fellows/">Open Web Fellows</Link>
-            </div>
-            <div className="nav-link-container">
-              <a href="">Blog</a>
-            </div>
-          </div>
+          {menu}
         </div>
       </div>
     );
--- pages/encrypt/default.jsx
+++ pages/encrypt/default.jsx
@@ -1,14 +1,29 @@
 var React = require('react');
 var Footer = require('../../components/footer.jsx');
 var Header = require('../../components/header.jsx');
+var HeroUnit = require('../../components/hero-unit.jsx');
+var Video = require('../../components/video.jsx');
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <div>
-        <Header></Header>
-        Default
-        <Footer></Footer>
+      <div className="encrypt">
+        <Header disabledMenu={true} height="30" width="105" logoImage={'/assets/mozilla-wordmark.svg'}></Header>
+        <HeroUnit>
+          <div className="encryptText">
+            <h1>hehe</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </HeroUnit>
+        <Video/>
+        <div className="encryptMeta">
+          <h2>
+            Why does encryption matter?
+          </h2>
+          <span className="videoMeta">FEBUARY 05, 2016  |  EPISODE 1  | 0:53</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <Footer/>
       </div>
     );
   }
