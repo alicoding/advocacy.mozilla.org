4c79741d0319dcdcef48767e46e3174909cd190a
Ali Al Dallal <ali@alicoding.com>
2016-01-31 09:53:39 -0500

    Add content

--- pages/encrypt/default.jsx
+++ pages/encrypt/default.jsx
@@ -3,6 +3,7 @@ var Footer = require('../../components/footer.jsx');
 var Header = require('../../components/header.jsx');
 var HeroUnit = require('../../components/hero-unit.jsx');
 var Video = require('../../components/video.jsx');
+var ContentContainer = require('../../components/content-container.jsx');
 
 module.exports = React.createClass({
   render: function() {
@@ -15,13 +16,15 @@ module.exports = React.createClass({
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </HeroUnit>
-        <Video/>
-        <div className="encryptMeta">
-          <h2>
-            Why does encryption matter?
-          </h2>
-          <span className="videoMeta">FEBUARY 05, 2016  |  EPISODE 1  | 0:53</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="page">
+          <Video/>
+          <ContentContainer className="encryptMeta">
+            <h2>
+              Why does encryption matter?
+            </h2>
+            <span className="videoMeta">FEBUARY 05, 2016  |  EPISODE 1  | 0:53</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </ContentContainer>
         </div>
         <Footer/>
       </div>
